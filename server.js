1846dbdabe703b1725f186d62e920b9d9ace5fb0
jhansiii <30890288+jhansiii@users.noreply.github.com>
2017-08-10 22:36:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,8 +102,17 @@ app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
     });
+    
+    var names=[];
+app.get('/submit-name',function(req,res){
+    var name=req.query.name;
+    
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
 
-app.get('/:articleNam', function (req, res) {
+
+app.get('/:articleName', function (req, res) {
    var  articleName = req.params.articleName;
  res.send(createTemplate(articles[articleName]));
 });
@@ -121,12 +130,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names=[];
-app.get('/submit-name/:name',function(req,res){
-    var name=req.params.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-});
 
 var port = 80;
 app.listen(port, function () {
