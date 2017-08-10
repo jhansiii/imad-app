9db72157014bc493e6fa56394a24d7df42dd1d68
jhansiii <30890288+jhansiii@users.noreply.github.com>
2017-08-10 15:43:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/artone', function (req, res) {
+  res.send("article 1 is being served here");
+});
+app.get('/arttwo', function (req, res) {
+  res.send("article 2 is being served here");
+});
+app.get('/artthree', function (req, res) {
+  res.send("article 3 is being served here");
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
