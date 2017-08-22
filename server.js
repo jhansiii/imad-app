bdc097a2c5c24cf1e36e94c0160f53c4371f94e0
jhansiii <30890288+jhansiii@users.noreply.github.com>
2017-08-22 22:07:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ var htmlTemplate=  `
               ${heading}
             </h3>
             <div>
-               ${date}
+               ${date.toDateString()}
             </div>
             <div>
             ${content}
@@ -133,9 +133,19 @@ app.get('/submit-name',function(req,res){
 });
 
 
-app.get('/:articleName', function (req, res) {
-   var  articleName = req.params.articleName;
- res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName', function (req, res) {
+   pool.query("SELECT * FROM article WHERE title = ' "+req.params.articleName + "'",function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       }else{
+           if(result.rows.length===0){
+               res.status(404).send('Article not found');
+           }else{
+               var articleData = result.rows[0];
+ res.send(createTemplate(articleData));
+           }
+       }
+   });
 });
 
 
