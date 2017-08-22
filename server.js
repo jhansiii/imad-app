049404cdeff5f98494e841c90a711235c83a878f
jhansiii <30890288+jhansiii@users.noreply.github.com>
2017-08-22 21:35:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ pool.query('SELECT * FROM test',function(err,result){
         res.status(500).send(err.toString());
     }
     else{
-        res.send(JSON.stringify(result));
+        res.send(JSON.stringify(result.rows));
     }
     
 });    
