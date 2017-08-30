b79364e19a77251fc3657893dd890360efc41b09
jhansiii <30890288+jhansiii@users.noreply.github.com>
2017-08-30 20:15:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,3 @@
-var button=document.getElementById('counter');
-
-button.onclick =function(){
-    
-    var request=new XMLHttpRequest();
-    
-    request.onreadystatechange=function(){
-        if(request.readystate===XMLHttpRequest.DONE){
-            if(request.status === 200){
-                 var counter=request.responseText;
-                 var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
-    
-                
-            }        }
-    };
-   request.open('GET','http://jhansinambala.imad.hasura-app.io/counter',true);
-   request.send(null);
-   
-};
 
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
