f5698e3cba83b29995f9397011445367360a93c3
jhansiii <30890288+jhansiii@users.noreply.github.com>
2017-08-30 21:15:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,7 +18,7 @@ submit.onclick = function() {
             }
     };
     
-    var username=document.getElementById('username').value;
+    var username=document.getElementById('Username').value;
 var password=document.getElementById('password').value;
 console.log(username);
 console.log(password);
